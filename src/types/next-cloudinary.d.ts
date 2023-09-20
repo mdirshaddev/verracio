ce030f73441e892ae56f658687b5d9cfc6b9e956
mdirshaddev <mdirshaddev+github@gmail.com>
2023-09-20 22:49:06 +0530

    types: typing file for cloudinary image component

---
+++ src/types/next-cloudinary.d.ts
@@ -0,0 +1,15 @@
+export type NextCloudinaryImgType = {
+  publicId: string;
+  height: number;
+  width: number;
+  alt: string;
+  title?: string;
+  className?: string;
+  preview?: boolean;
+  noStyle?: boolean;
+  aspect?: {
+    width: number;
+    height: number;
+  };
+  mdx?: boolean;
+} & React.ComponentPropsWithoutRef<"figure">;

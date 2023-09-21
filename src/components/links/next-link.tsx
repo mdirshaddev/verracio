de02dc19f0b317599e2105db099f5aac58af6d52
mdirshaddev <mdirshaddev+github@gmail.com>
2023-09-21 22:02:19 +0530

    feat: 🛠️ custom link extended from next link component

---
+++ src/components/links/next-link.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { cn } from "shared-lib/utils";
+
+import Link, { LinkProps } from "next/link";
+
+export type NextLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  openNewTab?: boolean;
+  className?: string;
+} & React.ComponentPropsWithoutRef<"a"> &
+  LinkProps;
+
+/**
+ * The NextLink component is a wrapper that conditionally renders either a Next.js Link or a regular
+ * anchor tag based on the href and openNewTab props.
+ * @returns The `NextLink` component returns either a `Link` component or an `a` element depending on
+ * the value of the `openNewTab` prop and the `href` prop. If `openNewTab` is `true` or if `href` does
+ * not start with '/' or '#', then a `Link` component is returned. Otherwise, an `a` element with
+ * `target
+ */
+const NextLink: React.FC<NextLinkProps> = (props) => {
+  const { children, className = "", href, openNewTab, ...rest } = props;
+
+  const isNewTab =
+    openNewTab !== undefined
+      ? openNewTab
+      : href && !href.startsWith("/") && !href.startsWith("#");
+
+  if (!isNewTab) {
+    return (
+      <Link href={href} {...rest} className={className}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      {...rest}
+      className={cn(className, "cursor-newtab")}
+    >
+      {children}
+    </a>
+  );
+};
+
+export { NextLink };

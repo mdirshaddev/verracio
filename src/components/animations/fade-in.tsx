537cd2f0bd5c1c8e340460ffdd28999f14989b8e
mdirshaddev <mdirshaddev+github@gmail.com>
2023-09-23 09:19:21 +0530

    feat: 🪄 fadein animation component

---
+++ src/components/animations/fade-in.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+// animation typings
+import type { FadeInContainerProps } from 'src/types/animations';
+
+// shared lib for utils
+import { cn } from 'shared-lib/utils';
+
+// hook for detecting load of component
+import { useLoaded } from 'src/modules/hooks/useLoaded';
+
+/**
+ * The FadeInContainer component is a TypeScript React component that conditionally applies a fade-in
+ * animation class to its children based on the isLoaded state.
+ * @param props - The `props` parameter is an object that contains the properties passed to the
+ * `FadeInContainer` component. These properties can be accessed using destructuring assignment. In
+ * this case, the `className` and `children` properties are being extracted from the `props` object.
+ * @returns The `FadeInContainer` component is returning a JSX element.
+ */
+export const FadeInContainer: React.FC<FadeInContainerProps> = (
+  props
+): JSX.Element => {
+  const isLoaded = useLoaded();
+
+  const { className, children } = props;
+  return (
+    <div className={cn(isLoaded && 'fade-in-start', className)}>{children}</div>
+  );
+};
